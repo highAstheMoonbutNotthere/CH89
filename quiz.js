7b2d06f959bf54dab4c5c4090089006a4dc4bc9b
highAstheMoonbutNotthere <99829100+highAstheMoonbutNotthere@users.noreply.github.com>
2022-06-28 18:06:03 -0500

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,9 @@
+p1n=localStorage.getItem("name1");
+p1s=0;
+p2n=localStorage.getItem("name2");
+p2s=0;
+
+document.getElementById("p1n").innerHTML=p1n+" : ";
+document.getElementById("p2n").innerHTML=p2n+" : ";
+document.getElementById("p1s").innerHTML=p1s;
+document.getElementById("p2s").innerHTML=p2s;
